f53a1d87713222919e30a69453e5669175ea93a5
raatavares <ricardinhoalmeida2010@gmail.com>
2022-11-15 16:35:25 +0000

    Estruturas e ficheiros do prof

--- header.h
+++ header.h
@@ -4,11 +4,21 @@ char fileFI[20]="item.txt";
 #define FPROMOTERS fileFP
 #define FUSERS fileFU
 #define FITEMS fileFI
+#define TAM 50
 
 typedef struct utilizador{
-
-}
+    char username[TAM];
+    char password[TAM];
+    int saldo;
+}user;
 
 typedef struct items{
-    
-}
+    int IDitem;
+    char name[TAM];
+    char category[TAM];
+    int current_value;
+    int value;
+    int duration;
+    char user_sell[TAM];
+    char user_buyer[TAM];
+}item;
